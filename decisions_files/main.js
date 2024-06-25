f1d39b1904d803d87afba09565242b2b40a4bc87
Николай Коршунов <knn3477@gmail.com>
2024-06-25 03:25:15 +0300

    fix: фиксация восстановленного из архивариуса

---
+++ decisions_files/main.js
@@ -0,0 +1,216 @@
+var _____WB$wombat$assign$function_____ = function(name) {return (self._wb_wombat && self._wb_wombat.local_init && self._wb_wombat.local_init(name)) || self[name]; };
+if (!self.__WB_pmw) { self.__WB_pmw = function(obj) { this.__WB_source = obj; return this; } }
+{
+  let window = _____WB$wombat$assign$function_____("window");
+  let self = _____WB$wombat$assign$function_____("self");
+  let document = _____WB$wombat$assign$function_____("document");
+  let location = _____WB$wombat$assign$function_____("location");
+  let top = _____WB$wombat$assign$function_____("top");
+  let parent = _____WB$wombat$assign$function_____("parent");
+  let frames = _____WB$wombat$assign$function_____("frames");
+  let opener = _____WB$wombat$assign$function_____("opener");
+
+/*
+	Solid State by HTML5 UP
+	html5up.net | @n33co
+	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
+*/
+
+(function($) {
+
+	"use strict";
+
+	var _i = document.getElementById('header').getElementsByClassName('i')[0];
+	var _menu = document.getElementById('header').getElementsByTagName('div');
+	var _header = document.getElementById('header');
+
+	_i.onclick = function (target) {
+		for (var i=1; i<_menu.length; i++){
+			if (_menu[i].style.display=='none') {_menu[i].style.display='block'; _header.style.flexDirection='column';}
+			else {_menu[i].style.display='none'; _header.style.flexDirection='row'; _header.style.height='auto';}
+		};
+	}
+
+
+
+
+
+	skel.breakpoints({
+		xlarge:	'(max-width: 1680px)',
+		large:	'(max-width: 1280px)',
+		medium:	'(max-width: 980px)',
+		small:	'(max-width: 736px)',
+		xsmall:	'(max-width: 480px)'
+	});
+
+	$(function() {
+
+		var	$window = $(window),
+			$body = $('body'),
+			$header = $('#header'),
+			$banner = $('#banner');
+
+		 //Disable animations/transitions until the page has loaded.
+			//$body.addClass('is-loading');
+
+		//	$window.on('load', function() {
+			//	window.setTimeout(function() {
+				//	$body.removeClass('is-loading');
+				//}, 100);
+			//});
+
+		// Fix: Placeholder polyfill.
+			$('form').placeholder();
+
+		// Prioritize "important" elements on medium.
+			skel.on('+medium -medium', function() {
+				$.prioritize(
+					'.important\\28 medium\\29',
+					skel.breakpoint('medium').active
+				);
+			});
+
+/*		// Header.
+			if (skel.vars.IEVersion < 9)
+				$header.removeClass('alt');
+
+			if ($banner.length > 0
+			&&	$header.hasClass('alt')) {
+
+				$window.on('resize', function() { $window.trigger('scroll'); });
+
+				$banner.scrollex({
+					bottom:		$header.outerHeight(),
+					terminate:	function() { $header.removeClass('alt'); },
+					enter:		function() { $header.addClass('alt'); },
+					leave:		function() { $header.removeClass('alt'); }
+				});
+
+			}
+*/
+		// Menu.
+			var $menu = $('#menu');
+
+			$menu._locked = false;
+
+			$menu._lock = function() {
+
+				if ($menu._locked)
+					return false;
+
+				$menu._locked = true;
+
+				window.setTimeout(function() {
+					$menu._locked = false;
+				}, 350);
+
+				return true;
+
+			};
+
+			$menu._show = function() {
+
+				if ($menu._lock())
+					$body.addClass('is-menu-visible');
+
+			};
+
+			$menu._hide = function() {
+
+				if ($menu._lock())
+					$body.removeClass('is-menu-visible');
+
+			};
+
+			$menu._toggle = function() {
+
+				if ($menu._lock())
+					$body.toggleClass('is-menu-visible');
+
+			};
+
+			$menu
+				.appendTo($body)
+				.on('click', function(event) {
+
+					event.stopPropagation();
+
+					// Hide.
+						$menu._hide();
+
+				})
+				.find('.inner')
+					.on('click', '.close', function(event) {
+
+						event.preventDefault();
+						event.stopPropagation();
+						event.stopImmediatePropagation();
+
+						// Hide.
+							$menu._hide();
+
+					})
+					.on('click', function(event) {
+						event.stopPropagation();
+					})
+					.on('click', 'a', function(event) {
+
+						var href = $(this).attr('href');
+
+						event.preventDefault();
+						event.stopPropagation();
+
+						// Hide.
+							$menu._hide();
+
+						// Redirect.
+							window.setTimeout(function() {
+								window.location.href = href;
+							}, 350);
+
+					});
+
+			$body
+				.on('click', 'a[href="#menu"]', function(event) {
+
+					event.stopPropagation();
+					event.preventDefault();
+
+					// Toggle.
+						$menu._toggle();
+
+				})
+				.on('keydown', function(event) {
+
+					// Hide on escape.
+						if (event.keyCode == 27)
+							$menu._hide();
+
+				});
+
+	});
+
+})(jQuery);
+
+
+}
+/*
+     FILE ARCHIVED ON 04:19:11 Jan 13, 2022 AND RETRIEVED FROM THE
+     INTERNET ARCHIVE ON 00:00:27 Jun 25, 2024.
+     JAVASCRIPT APPENDED BY WAYBACK MACHINE, COPYRIGHT INTERNET ARCHIVE.
+
+     ALL OTHER CONTENT MAY ALSO BE PROTECTED BY COPYRIGHT (17 U.S.C.
+     SECTION 108(a)(3)).
+*/
+/*
+playback timings (ms):
+  captures_list: 1.237
+  exclusion.robots: 0.251
+  exclusion.robots.policy: 0.186
+  esindex: 0.015
+  cdx.remote: 18.147
+  LoadShardBlock: 67.907 (3)
+  PetaboxLoader3.datanode: 70.094 (4)
+  load_resource: 5074.137
+  PetaboxLoader3.resolve: 5031.284
+*/
---
+++ files/main.js
@@ -0,0 +1,217 @@
+var _____WB$wombat$assign$function_____ = function(name) {return (self._wb_wombat && self._wb_wombat.local_init && self._wb_wombat.local_init(name)) || self[name]; };
+if (!self.__WB_pmw) { self.__WB_pmw = function(obj) { this.__WB_source = obj; return this; } }
+{
+  let window = _____WB$wombat$assign$function_____("window");
+  let self = _____WB$wombat$assign$function_____("self");
+  let document = _____WB$wombat$assign$function_____("document");
+  let location = _____WB$wombat$assign$function_____("location");
+  let top = _____WB$wombat$assign$function_____("top");
+  let parent = _____WB$wombat$assign$function_____("parent");
+  let frames = _____WB$wombat$assign$function_____("frames");
+  let opener = _____WB$wombat$assign$function_____("opener");
+
+/*
+	Solid State by HTML5 UP
+	html5up.net | @n33co
+	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
+*/
+
+(function($) {
+
+	"use strict";
+
+	var _i = document.getElementById('header').getElementsByClassName('i')[0];
+	var _menu = document.getElementById('header').getElementsByTagName('div');
+	var _header = document.getElementById('header');
+
+	_i.onclick = function (target) {
+		for (var i=1; i<_menu.length; i++){
+			if (_menu[i].style.display=='none') {_menu[i].style.display='block'; _header.style.flexDirection='column';}
+			else {_menu[i].style.display='none'; _header.style.flexDirection='row'; _header.style.height='auto';}
+		};
+	}
+
+
+
+
+
+	skel.breakpoints({
+		xlarge:	'(max-width: 1680px)',
+		large:	'(max-width: 1280px)',
+		medium:	'(max-width: 980px)',
+		small:	'(max-width: 736px)',
+		xsmall:	'(max-width: 480px)'
+	});
+
+	$(function() {
+
+		var	$window = $(window),
+			$body = $('body'),
+			$header = $('#header'),
+			$banner = $('#banner');
+
+		 //Disable animations/transitions until the page has loaded.
+			//$body.addClass('is-loading');
+
+		//	$window.on('load', function() {
+			//	window.setTimeout(function() {
+				//	$body.removeClass('is-loading');
+				//}, 100);
+			//});
+
+		// Fix: Placeholder polyfill.
+			$('form').placeholder();
+
+		// Prioritize "important" elements on medium.
+			skel.on('+medium -medium', function() {
+				$.prioritize(
+					'.important\\28 medium\\29',
+					skel.breakpoint('medium').active
+				);
+			});
+
+/*		// Header.
+			if (skel.vars.IEVersion < 9)
+				$header.removeClass('alt');
+
+			if ($banner.length > 0
+			&&	$header.hasClass('alt')) {
+
+				$window.on('resize', function() { $window.trigger('scroll'); });
+
+				$banner.scrollex({
+					bottom:		$header.outerHeight(),
+					terminate:	function() { $header.removeClass('alt'); },
+					enter:		function() { $header.addClass('alt'); },
+					leave:		function() { $header.removeClass('alt'); }
+				});
+
+			}
+*/
+		// Menu.
+			var $menu = $('#menu');
+
+			$menu._locked = false;
+
+			$menu._lock = function() {
+
+				if ($menu._locked)
+					return false;
+
+				$menu._locked = true;
+
+				window.setTimeout(function() {
+					$menu._locked = false;
+				}, 350);
+
+				return true;
+
+			};
+
+			$menu._show = function() {
+
+				if ($menu._lock())
+					$body.addClass('is-menu-visible');
+
+			};
+
+			$menu._hide = function() {
+
+				if ($menu._lock())
+					$body.removeClass('is-menu-visible');
+
+			};
+
+			$menu._toggle = function() {
+
+				if ($menu._lock())
+					$body.toggleClass('is-menu-visible');
+
+			};
+
+			$menu
+				.appendTo($body)
+				.on('click', function(event) {
+
+					event.stopPropagation();
+
+					// Hide.
+						$menu._hide();
+
+				})
+				.find('.inner')
+					.on('click', '.close', function(event) {
+
+						event.preventDefault();
+						event.stopPropagation();
+						event.stopImmediatePropagation();
+
+						// Hide.
+							$menu._hide();
+
+					})
+					.on('click', function(event) {
+						event.stopPropagation();
+					})
+					.on('click', 'a', function(event) {
+
+						var href = $(this).attr('href');
+
+						event.preventDefault();
+						event.stopPropagation();
+
+						// Hide.
+							$menu._hide();
+
+						// Redirect.
+							window.setTimeout(function() {
+								window.location.href = href;
+							}, 350);
+
+					});
+
+			$body
+				.on('click', 'a[href="#menu"]', function(event) {
+
+					event.stopPropagation();
+					event.preventDefault();
+
+					// Toggle.
+						$menu._toggle();
+
+				})
+				.on('keydown', function(event) {
+
+					// Hide on escape.
+						if (event.keyCode == 27)
+							$menu._hide();
+
+				});
+
+	});
+
+})(jQuery);
+
+
+}
+/*
+     FILE ARCHIVED ON 16:02:40 Apr 01, 2022 AND RETRIEVED FROM THE
+     INTERNET ARCHIVE ON 08:53:28 Jun 18, 2024.
+     JAVASCRIPT APPENDED BY WAYBACK MACHINE, COPYRIGHT INTERNET ARCHIVE.
+
+     ALL OTHER CONTENT MAY ALSO BE PROTECTED BY COPYRIGHT (17 U.S.C.
+     SECTION 108(a)(3)).
+*/
+/*
+playback timings (ms):
+  captures_list: 2.326
+  exclusion.robots: 0.105
+  exclusion.robots.policy: 0.094
+  esindex: 0.013
+  cdx.remote: 239.557
+  LoadShardBlock: 81.981 (3)
+  PetaboxLoader3.datanode: 83.673 (5)
+  load_resource: 149.237
+  PetaboxLoader3.resolve: 99.573
+  loaddict: 38.068
+*/
